3cb9f126101579ebb9a8f4479122094acac8d502
Cryptho <pedro-gus1@hotmail.com>
2024-11-28 17:26:30 -0600

    Se mejora experiencia de usuario en notificacion

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,14 @@ self.addEventListener("notificationclick", event => {
 
     event.waitUntil(
         clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
-            if (clientList.length > 0) {
-
-                return clientList[0].focus();
+            const url = new URL(event.notification.data); 
+            for (const client of clientList) {
+                const clientUrl = new URL(client.url);
+                if (clientUrl.origin === url.origin && clientUrl.pathname === url.pathname) {
+                    if ("focus" in client) {
+                        return client.focus();
+                    }
+                }
             }
             if (clients.openWindow) {
                 return clients.openWindow(event.notification.data);
